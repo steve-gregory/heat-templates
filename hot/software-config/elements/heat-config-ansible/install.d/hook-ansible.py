90fc7d0980c782110fa325d2601d7e4ca4046bae
Steve Gregory <contact@steve-gregory.com>
2014-05-20 09:24:23 -0700

    Iteration 1 - Ready for testing of Ansible

---
+++ hot/software-config/elements/heat-config-ansible/install.d/hook-ansible.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import json
+import logging
+import os
+import subprocess
+import sys
+
+WORKING_DIR = os.environ.get('HEAT_ANSIBLE_WORKING',
+                             '/var/lib/heat-config/heat-config-ansible')
+OUTPUTS_DIR = os.environ.get('HEAT_ANSIBLE_OUTPUTS',
+                             '/var/run/heat-config/heat-config-ansible')
+
+
+def prepare_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path, 0o700)
+
+
+def main(argv=sys.argv):
+    log = logging.getLogger('heat-config')
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(
+        logging.Formatter(
+            '[%(asctime)s] (%(name)s) [%(levelname)s] %(message)s'))
+    log.addHandler(handler)
+    log.setLevel('DEBUG')
+
+    prepare_dir(OUTPUTS_DIR)
+    prepare_dir(WORKING_DIR)
+    os.chdir(WORKING_DIR)
+
+    c = json.load(sys.stdin)
+
+    env = os.environ.copy()
+    variables = {}
+    for input in c['inputs']:
+        input_name = input['name']
+        variables[input_name] = input.get('value','')
+
+    fn = os.path.join(WORKING_DIR, '%s_playbook.yaml' % c['id'])
+    heat_outputs_path = os.path.join(OUTPUTS_DIR, c['id'])
+    variables['heat_outputs_path'] = heat_outputs_path
+
+    with os.fdopen(os.open(fn, os.O_CREAT | os.O_WRONLY, 0o700), 'w') as f:
+        #TODO: What does this fn do?
+        f.write(c.get('config', ''))
+    cmd = ['ansible-playbook', fn]
+    log.debug('Running %s' % cmd)
+    try:
+        subproc = subprocess.Popen([cmd], stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, env=env)
+    except OSError:
+        log.warn("ansible not installed yet")
+        return
+    stdout, stderr = subproc.communicate()
+
+    log.info('Return code %s' % subproc.returncode)
+    if stdout:
+        log.info(stdout)
+    if stderr:
+        log.info(stderr)
+
+    #TODO: Test if ansible returns any non-zero return codes in success.
+    if subproc.returncode:
+        log.error("Error running %s. [%s]\n" % (fn, subproc.returncode))
+    else:
+        log.info('Completed %s' % fn)
+
+    response = {}
+
+    for output in c.get('outputs') or []:
+        output_name = output['name']
+        try:
+            with open('%s.%s' % (heat_outputs_path, output_name)) as out:
+                response[output_name] = out.read()
+        except IOError:
+            pass
+
+    response.update({
+        'deploy_stdout': stdout,
+        'deploy_stderr': stderr,
+        'deploy_status_code': subproc.returncode,
+    })
+
+    json.dump(response, sys.stdout)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
